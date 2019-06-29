0a78c26d81a6a35560a13479b49212c0c6275a98
Vale the Violet Mote <valethemajor@gmail.com>
2019-06-29 19:07:22 -0400

    attempted adjustment to fit vmangos' better

--- GlobalMethods.h
+++ GlobalMethods.h
@@ -398,7 +398,7 @@ namespace LuaGlobalFunctions
         if (ItemLocale const* il = eObjectMgr->GetItemLocale(entry))
             {
                 #ifdef VMANGOS
-                ObjectMgr::GetAreaLocaleString(il->Name, static_cast<LocaleConstant>(locale), name);
+                ObjectMgr::GetAreaLocaleString(entry, static_cast<LocaleConstant>(locale), name);
                 #else
                 ObjectMgr::GetLocaleString(il->Name, static_cast<LocaleConstant>(locale), name);
                 #endif
